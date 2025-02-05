af9986645076ba9ab7547be3f4652e8d7be3b5f1
Cenk Öztürk <ccenkozturrk@outlook.com>
2025-02-05 20:32:30 +0300

    Rearranged Order class in GR

--- GenericRepository/Order.cs
+++ GenericRepository/Order.cs
@@ -11,6 +11,6 @@ namespace GenericRepository
         public int Id { get; set; }
         public string ProductName { get; set; } = string.Empty;
         public int Quantity { get; set; }
-        }
+    }
 
 }
